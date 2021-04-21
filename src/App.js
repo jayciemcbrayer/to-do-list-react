952545f5d2ad8c2935f4b84911c4432f37a346ad
Jaycie McBrayer <jaycielaynee@gmail.com>
2021-04-21 15:24:49 -0500

    progress on all three components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import Header from './components/Header';
+import ToDoList from './components/ToDoList';
+import { useState } from 'react';
+
 
 function App() {
+  const [toDoList, setToDoList] = useState(
+    [
+    {
+      id: 1,
+      toDoItem: "Study React for 1 hour",
+      dueDate: "daily",
+      complete: false
+
+    },
+    {
+      id: 2,
+      toDoItem: "Meal Prep",
+      dueDate: "4/25/21",
+      complete: false
+
+    },
+    {
+      id: 3,
+      toDoItem: "Work on Portfolio Site",
+      dueDate: "daily",
+      complete: false
+
+    }
+    ]
+  )
   return (
-    <div>
-      
+    <div className="App">
+      <Header />
+      <ToDoList toDoList={toDoList}/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './Button'
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1>To Do List</h1>
+      <Button/>
+    </div>
+  )
+}
+
+export default Header
